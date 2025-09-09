989769ad879395507e817019b389aef2c8f1cd2d
Nyx <service@obscuraworks.com>
2025-09-09 08:20:37 +0700

    Create facebook.js

---
+++ downloader/facebook.js
@@ -0,0 +1,41 @@
+/**
+ * Downloads Facebook content using the ObscuraWorks API.
+ *
+ * This script fetches media data from a specified Facebook URL
+ * through the ObscuraWorks downloader endpoint. Make sure your API key
+ * is valid and your IP address has been whitelisted in the dashboard.
+ *
+ * @note You must whitelist your current IP address in the dashboard at:
+ *       https://docs.obscuraworks.com/dashboard/profile
+ *       Otherwise, the request may fail due to unauthorized access.
+ *
+ * @see API documentation: https://docs.obscuraworks.com
+ *
+ * @constant {string} url - The complete API URL including the target Facebook.
+ * @constant {Object} headers - HTTP request headers containing your ObscuraWorks API key.
+ *
+ * @function fetchInstagramReel
+ * @async
+ * @returns {Promise<Object|void>} The parsed JSON response from the API,
+ *                                 or logs an error if the request fails.
+ */
+const fetch = require("node-fetch");
+
+const url = "https://api.obscuraworks.com/api/downloader/facebook?url=https://www.facebook.com/share/v/1HgNcDXaN1/";
+
+(async () => {
+  try {
+    const res = await fetch(url, {
+      headers: {
+        "Authorization": "Bearer YOUR_API_KEY",
+        "Accept": "application/json"
+      }
+    });
+
+    const data = await res.json();
+    return data;
+    console.log(JSON.stringify(data, null, 2));
+  } catch (err) {
+    console.error("Request failed:", err.message);
+  }
+})();
